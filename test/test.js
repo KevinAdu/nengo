00d8075ca969330b45f10e2b97ecab612de6f169
KevinAdu <kevyugi@gmail.com>
2018-06-05 22:34:38 +0900

    Added test for the current calendar year

--- test/test.js
+++ test/test.js
@@ -5,14 +5,20 @@ const yearConverter = require('../index');
 
 describe('#yearConverter', () => {
   it('should convert the Gregorian year to the appropriate Japanese calendar year', () => {
-      const heiseiYear = yearConverter(1990);
       const heiseiExactYear = yearConverter(1989);
       const showaYear = yearConverter(1987);
       const meijiExactYear = yearConverter(1868);
 
-      expect(heiseiYear).to.equal('Heisei');
       expect(heiseiExactYear).to.equal('Heisei');
       expect(showaYear).to.equal('Showa');
       expect(meijiExactYear).to.equal('Meiji');
   });
+
+  it('should convert any year above the last emperor calendar date', () => {
+    const heiseiYear = yearConverter(1990);
+    const tokyoOlympicYear = yearConverter(2020);
+
+    expect(heiseiYear).to.equal('Heisei');
+    expect(tokyoOlympicYear).to.equal('Heisei');
+  })
 });
