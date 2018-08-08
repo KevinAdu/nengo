8ef7baf969eb228ffed955ddb22f4662b0cc1124
KevinAdu <kevyugi@gmail.com>
2018-08-09 07:12:07 +0900

    Changed the structure of periods json

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const periodData = require("./periods");
 
-/**
- * Converts Gregorian calendar year to Japanese calendar year
- * @param {number} year
- * @return {string}
- */
 module.exports = {
+  /**
+   * Converts Gregorian calendar year to Japanese calendar year
+   * @param {number} year
+   * @return {string}
+   */
   japaneseYear: function(gregorianYear) {
-    const periodYears = Object.keys(periodData).sort((a, b) => b - a);
+    const periodYears = periodData.sort((a, b) => b.startYear - a.startYear).map(period => period.startYear);
     const exactPeriodYear = periodYears.find((periodYear, i) => {
       if (i === 0) return periodYear <= gregorianYear;
       if (i === periodYears.length - 1) return true;
       return periodYear <= gregorianYear && periodYears[i - 1] > gregorianYear;
     });
 
-    return periodData[exactPeriodYear];
+    return periodData.find(period => exactPeriodYear == period.startYear);
   }
 }
